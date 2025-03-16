0c94893b50e8069b62f953523cff944e3a4c28ac
Manolo Alvarez <35587627+malvarez0722@users.noreply.github.com>
2025-03-15 21:43:59 -0700

    added model and cleaned up

--- kos_sim/server.py
+++ kos_sim/server.py
@@ -180,8 +180,8 @@ async def serve(
     #         get_model_metadata(api, model_name),
     #     )
     # TODO: remove this:
-    model_dir = Path("/Users/manoloalvarez/playground/Minimal-Locomotion/kos-sim-cs234/assets/zbot-v2-fixed/robot")
-    model_path = Path("/Users/manoloalvarez/playground/Minimal-Locomotion/kos-sim-cs234/.kos-sim/zbot-v2-fixed/metadata.json")
+    model_dir = Path("kos-sim-cs234/assets/zbot-v2-fixed/robot")
+    model_path = Path("kos-sim-cs234/.kos-sim/zbot-v2-fixed/metadata.json")
     model_metadata = RobotURDFMetadataOutput.model_validate_json(model_path.read_text())
     model_path = next(
         itertools.chain(
